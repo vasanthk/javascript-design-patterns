9ad46bb60b43b889061a6f74fc70290a04013d19
vasanthk <vasa.httpster@gmail.com>
2017-01-14 13:22:48 -0800

    Decorator pattern - more notes

--- decorator-pattern.js
+++ decorator-pattern.js
@@ -1,7 +1,8 @@
 /**
  * Decorator Pattern
  *
- * Dynamically add functionality to JavaScript objects at runtime
+ * Dynamically add functionality to JavaScript objects at runtime.
+ * Decorators provide a flexible alternative to subclassing for extending functionality.
  *
  * @Reference:
  * http://www.drdobbs.com/web-development/decorator-pattern-in-javascript/232200406
