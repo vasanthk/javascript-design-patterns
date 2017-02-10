69c98fac345f24d0d0677b022b829ddc4daff8c6
vasanthk <vasa.httpster@gmail.com>
2017-02-09 19:51:50 -0800

    More links

--- decorator-pattern.js
+++ decorator-pattern.js
@@ -9,4 +9,5 @@
  * http://www.dofactory.com/javascript/decorator-design-pattern
  * http://nickmeldrum.com/blog/decorators-in-javascript-using-monkey-patching-closures-prototypes-proxies-and-middleware
  * https://miguelmota.com/blog/exploring-decorators-in-javascript/
+ * https://www.safaribooksonline.com/library/view/learning-javascript-design/9781449334840/ch09s14.html
  */
