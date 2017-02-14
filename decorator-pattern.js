8d592bc10adadb7f4644c3bfd5eee6bc92390b35
vasanthk <vasa.httpster@gmail.com>
2017-02-13 22:02:17 -0800

    More on decorators

--- decorator-pattern.js
+++ decorator-pattern.js
@@ -4,6 +4,10 @@
  * Dynamically add functionality to JavaScript objects at runtime.
  * Decorators provide a flexible alternative to subclassing for extending functionality.
  *
+ * A convenient feature of the decorator pattern is the customization and configuration of the expected behavior.
+ * You start with your plain object, which has some basic functionality.
+ * Then you pick and choose from an available pool of decorators which ones you want to use to enhance your plain object and in which order, if the order is important.
+ *
  * @Reference:
  * http://www.drdobbs.com/web-development/decorator-pattern-in-javascript/232200406
  * http://www.dofactory.com/javascript/decorator-design-pattern
